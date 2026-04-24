be5724c6dfed1cea33ff2cba6ac9f49642da104b
Dmanhz <duymanhbui305@gmail.com>
2026-04-24 15:26:25 +0700

    update_logic_Customer_24521040

---
+++ Views/Customer/DeleteConfirmWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace test2.Views.Model_Box
+{
+    public partial class DeleteConfirmWindow : Window
+    {
+        public DeleteConfirmWindow(string customerName)
+        {
+            InitializeComponent();
+            txtMessage.Text = $"Bạn có chắc chắn muốn xóa khách hàng {customerName} không? Hành động này không thể hoàn tác.";
+        }
+
+        private void Confirm_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+    }
+}
